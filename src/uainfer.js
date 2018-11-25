c7f6014b3252a8728d7251b78eeccfb19fc67749
Ariya Hidayat <ariya.hidayat@gmail.com>
2018-11-25 00:05:06 -0800

    Refactor the tokenization

--- src/uainfer.js
+++ src/uainfer.js
@@ -75,16 +75,12 @@
     }
 
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
-    function scan(UserAgentString) {
-        var tokens = [];
-
+    function tokenize(UserAgentString, handler) {
         function append(part) {
             if (part.length > 0) {
                 var parts = part.split('/');
-                tokens.push({
-                    name: parts[0],
-                    version: (parts.length > 1) ? parts[1] : ''
-                });
+                var version = (parts.length > 1) ? parts[1] : '';
+                handler(parts[0], version);
             }
         }
 
@@ -108,9 +104,7 @@
                 }
                 var hints = hint.split(';');
                 for (var h = 0; h < hints.length; ++h) {
-                    tokens.push({
-                        name: hints[h].trim()
-                    });
+                    handler(hints[h].trim());
                 }
                 ++pos;
             } else if (ch !== ' ' && ch !== '(') {
@@ -118,7 +112,16 @@
             }
         }
         append(part);
+    }
 
+    function scan(UserAgentString) {
+        var tokens = [];
+        tokenize(UserAgentString, function (name, version) {
+            tokens.push({
+                name: name,
+                version: version
+            });
+        });
         return tokens;
     }
 
@@ -128,65 +131,66 @@
         var chromeFamily = null;
         var os = { name: null, version: null };
 
+        var mozVersion = null;
         var hasGecko = false;
         var hasWebKit = false;
-        var versionProduct = null;
-        var mobileProduct = null;
+        var hasMobile = false;
+        var mobileMarker = null;
+        var versionMarker = null;
         var tailSafari = false;
 
-        var tokens = scan(UserAgentString);
-        for (var i = 0; i < tokens.length; ++i) {
-            var t = tokens[i];
-            if (typeof t.version === 'string') {
-                if (t.name === 'Mozilla' && i === 0 && (['4.0', '5.0'].indexOf(t.version) < 0)) {
-                    break;
-                }
-                tailSafari = hasWebKit && (t.name === 'Safari');
-                hasWebKit |= (t.name === 'AppleWebKit');
-                hasGecko |= (t.name === 'Gecko');
-                versionProduct = (t.name === 'Version') ? t : versionProduct;
-                mobileProduct = (t.name === 'Mobile') ? t : mobileProduct;
-                if (hasGecko && t.name === 'Firefox') {
+        var index = 0;
+        tokenize(UserAgentString, function (name, version) {
+            ++index;
+            if (typeof version === 'string') {
+                mozVersion = (index === 1) ? version : mozVersion;
+                tailSafari = hasWebKit && (name === 'Safari');
+                hasWebKit |= (name === 'AppleWebKit');
+                hasGecko |= (name === 'Gecko');
+                hasMobile |= (name === 'Mobile');
+                mobileMarker = (name === 'Mobile') ? version : mobileMarker;
+                versionMarker = (name === 'Version') ? version : versionMarker;
+                if (hasGecko && name === 'Firefox') {
                     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
                     browserName = 'Firefox';
-                    browserVersion = t.version;
-                } else if (hasWebKit && t.name === 'Chrome') {
+                    browserVersion = version;
+                } else if (hasWebKit && name === 'Chrome') {
                     // https://developer.chrome.com/multidevice/user-agent
                     chromeFamily = {
-                        version: parseInt(t.version, 10),
-                        fullVersion: t.version
+                        version: parseInt(version, 10),
+                        fullVersion: version
                     };
-                } else if (hasWebKit && t.name === 'CriOS') {
+                } else if (hasWebKit && name === 'CriOS') {
                     browserName = 'Chrome';
-                    browserVersion = t.version;
-                } else if (t.name === 'Edge' || t.name === 'EdgA' || t.name === 'EdgiOS') {
+                    browserVersion = version;
+                } else if (name === 'Edge' || name === 'EdgA' || name === 'EdgiOS') {
                     browserName = 'Edge';
-                    browserVersion = t.version;
-                    if (t.name !== 'EdgA') {
+                    browserVersion = version;
+                    if (name !== 'EdgA') {
                         // Only Edge on Android is based on Chrome/WebView/Blink:
                         // https://blogs.windows.com/msedgedev/2017/10/05/microsoft-edge-ios-android-developer/
                         chromeFamily = null;
                     }
-                } else if (hasWebKit && t.version) {
-                    var name = chromeBased[t.name];
-                    var validVersion = t.version[0] >= '0' && t.version[0] <= '9';
+                } else if (hasWebKit && version) {
+                    var name = chromeBased[name];
+                    var validVersion = version[0] >= '0' && version[0] <= '9';
                     if (name && validVersion) {
                         browserName = name;
-                        browserVersion = t.version;
+                        browserVersion = version;
                     }
                 }
             } else {
-                if (t.name.substr(0, 10) === 'Windows NT') {
-                    var ver = osNames[t.name];
+                if (name.substr(0, 10) === 'Windows NT') {
+                    var ver = osNames[name];
                     os.version = ver ? ver : null;
                     os.name = 'Windows';
-                } else if (t.name.substr(0, 5) === 'MSIE ') {
+                } else if (name.substr(0, 5) === 'MSIE ') {
                     // This is mostly for Internet Explorer 7:
                     // https://blogs.msdn.microsoft.com/ie/2005/04/28/internet-explorer-7-user-agent-string/
                     browserName = 'Internet Explorer';
-                    browserVersion = t.name.substr(5);
-                } else if (t.name.substr(0, 8) === 'Trident/') {
-                    var tridentVersion = parseInt(t.name.substr(8));
+                    browserVersion = name.substr(5);
+                } else if (name.substr(0, 8) === 'Trident/') {
+                    var tridentVersion = parseInt(name.substr(8));
                     if (tridentVersion >= 4 && tridentVersion < 12) {
                         // https://blogs.msdn.microsoft.com/ie/2009/01/09/the-internet-explorer-8-user-agent-string-updated-edition/
                         // https://blogs.msdn.microsoft.com/ie/2011/04/15/the-ie10-user-agent-string/
@@ -194,42 +198,47 @@
                         browserName = 'Internet Explorer';
                         browserVersion = (tridentVersion + 4).toString();
                     }
-                } else if (t.name.indexOf('Mac OS X 10') >=  4) {
-                    var info = t.name.substr(t.name.indexOf('OS X') + 5);
+                } else if (name.indexOf('Mac OS X 10') >=  4) {
+                    var info = name.substr(name.indexOf('OS X') + 5);
                     var key = (info.indexOf('_') > 1) ? info.split('_') : info.split('.');
                     key = (key.length >= 2) ? key.slice(0, 2) : key.concat([0]);
                     os.name = parseInt(key[1], 10) >= 12 ? 'macOS' : 'OS X';
                     os.version = osNames[key.join('.')] ? key.join('.') + ' (' + osNames[key.join('.')] + ')' : key.join('.');
-                } else if (t.name.substr(0, 8) === 'Android ') {
+                } else if (name.substr(0, 8) === 'Android ') {
                     os.name = 'Android';
-                    os.version = t.name.substr(8).split('.').slice(0, 2).join('.');
-                } else if (t.name.substr(0, 7) === 'CPU OS ') {
+                    os.version = name.substr(8).split('.').slice(0, 2).join('.');
+                } else if (name.substr(0, 7) === 'CPU OS ') {
                     os.name = 'iOS';
-                    os.version = t.name.substr(7).replace(' like Mac OS X', '').split('_').slice(0, 2).join('.');
-                } else if (t.name.substr(0, 14) === 'CPU iPhone OS ') {
+                    os.version = name.substr(7).replace(' like Mac OS X', '').split('_').slice(0, 2).join('.');
+                } else if (name.substr(0, 14) === 'CPU iPhone OS ') {
                     os.name = 'iOS';
-                    os.version = t.name.substr(14).replace(' like Mac OS X', '').split('_').slice(0, 2).join('.');
-                } else if (t.name.substr(0, 6) === 'Tizen ') {
+                    os.version = name.substr(14).replace(' like Mac OS X', '').split('_').slice(0, 2).join('.');
+                } else if (name.substr(0, 6) === 'Tizen ') {
                     browserName = 'Tizen';
-                    browserVersion = t.name.substr(6);
+                    browserVersion = name.substr(6);
                 } else {
                     var genericOS = ['Linux', 'FreeBSD', 'OpenBSD', 'NetBSD', 'SunOS', 'CrOS'];
                     for (var s = 0; s < genericOS.length; ++s) {
-                        if (t.name.substr(0, genericOS[s].length) === genericOS[s]) {
+                        if (name.substr(0, genericOS[s].length) === genericOS[s]) {
                             os.name = (genericOS[s] === 'CrOS') ? 'ChromeOS' : genericOS[s];
                         }
                     }
                 }
             }
-        }
+        });
 
-        if (tailSafari && versionProduct && (!mobileProduct || mobileProduct.version)) {
+        if (tailSafari && versionMarker && (!hasMobile || mobileMarker)) {
             browserName = 'Safari';
-            browserVersion = versionProduct.version;
+            browserVersion = versionMarker;
         } else if (!browserVersion && chromeFamily) {
             // Fallback to Chrome, if no known Chrome impersonator is detected.
             browserName = 'Chrome';
             browserVersion = chromeFamily.fullVersion;
+        }
+
+        if (mozVersion !== '4.0' && mozVersion !== '5.0') {
+            browserName = 'Unknown';
+            browserVersion = null;
         }
 
         return new UserAgent(browserName, browserVersion, chromeFamily, os);
